7015f4d43d5545a10bac40cb22ae0ce9b9780615
Matias Sepulveda <matias.seto@gmail.com>
2023-08-01 23:04:57 -0400

    Hola mundo de Solana

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -4,12 +4,11 @@ declare_id!("9uJeg9wfWjti7chiHsJS7M289JsyxtjHCpAGFGf4amRt");
 
 #[program]
 pub mod myepicproject {
-    use super::*;
-
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        Ok(())
-    }
+  use super::*;
+  pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    Ok(())
+  }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct StartStuffOff {}
--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,23 @@
-const anchor = require("@coral-xyz/anchor");
+const anchor = require('@project-serum/anchor');
 
-describe("myepicproject", () => {
-  // Configure the client to use the local cluster.
+const main = async() => {
+  console.log("🚀 Starting test...")
+
   anchor.setProvider(anchor.AnchorProvider.env());
+  const program = anchor.workspace.Myepicproject;
+  const tx = await program.rpc.startStuffOff();
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const program = anchor.workspace.Myepicproject;
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-  });
-});
+  console.log("📝 Your transaction signature", tx);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+runMain();
