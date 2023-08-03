d9145931887e4fbf5893355fda5c822442a01cf9
Matias Sepulveda <matias.seto@gmail.com>
2023-08-03 17:36:48 -0400

    Guardar datos basicos en programa

--- programs/myepicproject/src/lib.rs
+++ programs/myepicproject/src/lib.rs
@@ -12,7 +12,17 @@ pub mod myepicproject {
     base_account.total_gifs = 0;
     Ok(())
   }
+
+	// Another function woo!
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    // Get a reference to the account and increment total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
+    Ok(())
+  }
+
 }
+
 
 // Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
@@ -23,6 +33,15 @@ pub struct StartStuffOff<'info> {
     pub user: Signer<'info>,
     pub system_program: Program <'info, System>,
 }
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
 
 // Tell Solana what we want to store on this account.
 #[account]
