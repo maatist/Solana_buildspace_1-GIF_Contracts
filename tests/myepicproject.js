3fe5957302e6e38cebeb106b59a5d01ebafaf676
Matias Sepulveda <matias.seto@gmail.com>
2023-08-02 23:05:37 -0400

    Guardar datos de GIFs

--- tests/myepicproject.js
+++ tests/myepicproject.js
@@ -1,13 +1,35 @@
 const anchor = require('@project-serum/anchor');
 
+// Esto lo usamos mas tarde
+const { SystemProgram } = anchor.web3;
+
 const main = async() => {
   console.log("🚀 Starting test...")
 
-  anchor.setProvider(anchor.AnchorProvider.env());
+  // Se crea y setea el provider. Lo hicimos antes pero hay que updatearlo para poder comunicarse con el Front
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+
   const program = anchor.workspace.Myepicproject;
-  const tx = await program.rpc.startStuffOff();
+	
+  // Se crea el keypair para que el programa lo use
+  const baseAccount = anchor.web3.Keypair.generate();
+
+  // Llamamos start_stuff_off, pass it the params it needs!
+  let tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log("📝 Your transaction signature", tx);
+
+  // Fetch data from the account.
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString())
 }
 
 const runMain = async () => {
